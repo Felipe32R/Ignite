a0caaab0a2d1b71ab0b926144f79be391d29ff0e
Felipe Ramalho <feliperamalhoy@gmail.com>
2022-01-08 14:48:41 -0300

    styles

--- react/01-github-explorer/src/index.tsx
+++ react/01-github-explorer/src/index.tsx
@@ -2,4 +2,7 @@
 import {render} from "react-dom";
 import { App } from "./App";
 
-render(<h1>Tesste</h1>, document.getElementById('root'))
+render(
+  <App/>, 
+  document.getElementById('root')
+);
